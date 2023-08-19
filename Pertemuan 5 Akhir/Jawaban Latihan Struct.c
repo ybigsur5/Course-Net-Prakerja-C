57dc8256e941718b928a88bcc4dd24d3ae699f85
Avira <115338956+ybigsur5@users.noreply.github.com>
2023-08-19 21:42:57 +0700

    Jawaban Latihan Struct.c

--- Pertemuan 5 Akhir/Jawaban Latihan Struct.c
+++ Pertemuan 5 Akhir/Jawaban Latihan Struct.c
@@ -53,6 +53,51 @@ void displayInventory(struct Item items[], int numItems) {
     }
 }
 
+void updateItem(struct Item items[], int numItems) {
+    if (numItems == 0) {
+        printf("No data.\n");
+        return;
+    }
+
+    displayInventory(items, numItems);
+
+    int choice;
+    do {
+        printf("Select the item to update [1..%d] (0 to cancel): ", numItems);
+        scanf("%d", &choice);
+    } while (choice < 0 || choice > numItems);
+
+    if (choice == 0) {
+        return;
+    }
+
+    choice--;
+
+    char name[16];
+    int quantity, price;
+
+    do {
+        printf("Input Item's name [5..15]: ");
+        scanf("%s", name);
+    } while (strlen(name) < 5 || strlen(name) > 15);
+
+    do {
+        printf("Input Item's quantity [1..100]: ");
+        scanf("%d", &quantity);
+    } while (quantity < 1 || quantity > 100);
+
+    do {
+        printf("Input Item's price [10000..100000]: ");
+        scanf("%d", &price);
+    } while (price < 10000 || price > 100000);
+
+    strcpy(items[choice].name, name);
+    items[choice].quantity = quantity;
+    items[choice].price = price;
+
+    printf("%s's information has been successfully updated\n", name);
+}
+
 int main() {
     struct Item inventory[10];
     int numItems = 0;
@@ -62,9 +107,10 @@ int main() {
         printf("\nCourse-Net Mart\n");
         printf("----------------------\n");
         printf("1. Add Item\n");
-        printf("2. Sell Item\n");
-        printf("3. Exit\n\n");
-        printf("Enter Your Choice [1..3]: ");
+        printf("2. Update Item\n");
+        printf("3. Sell Item\n");
+        printf("4. Exit\n\n");
+        printf("Enter Your Choice [1..4]: ");
         scanf("%d", &choice);
 
         switch (choice) {
@@ -72,15 +118,18 @@ int main() {
                 addItem(inventory, &numItems);
                 break;
             case 2:
-                displayInventory(inventory, numItems);
+                updateItem(inventory, numItems);
                 break;
             case 3:
+                displayInventory(inventory, numItems);
+                break;
+            case 4:
                 printf("Exiting the program.\n");
                 exit(0);
             default:
-                printf("Invalid choice. Please select between 1 to 3.\n");
+                printf("Invalid choice. Please select between 1 to 4.\n");
         }
-    } while (choice != 3);
+    } while (choice != 4);
 
     return 0;
 }
